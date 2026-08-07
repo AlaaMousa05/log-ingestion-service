d86775f067707e214a040544877c05e520c9331c
AlaaMousa05 <alaa.mousa025@gmail.com>
2026-08-07 22:23:18 +0300

    add fastify server foundation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-const message: string = "Log Ingestion and Query Service";
+import Fastify from "fastify";
 
-console.log(message);
+const app = Fastify({
+  logger: true,
+});
+
+app.get("/health", async () => {
+  return { status: "ok" };
+});
+
+const start = async (): Promise<void> => {
+  try {
+    await app.listen({
+      host: "0.0.0.0",
+      port: 8080,
+    });
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+};
+
+void start();
